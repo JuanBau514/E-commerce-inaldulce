0c954e4776b57cc1abb2664a76785954b95db427
DeynnerASR <rojasdeynner8@gmail.com>
2024-10-08 09:34:11 -0500

    BD actualizada

--- db/InadulcesBD.sql
+++ db/InadulcesBD.sql
@@ -9,55 +9,34 @@ CREATE TABLE genero (
     PRIMARY KEY(id_genero)
 );
 
-CREATE TABLE cargo (
-	id_cargo INT NOT NULL,
-	cargo VARCHAR(50),
-    PRIMARY KEY(id_cargo)
+CREATE TABLE rol (
+	id_rol INT NOT NULL,
+	rol VARCHAR(50),
+    PRIMARY KEY(id_rol)
 );
 
-CREATE TABLE persona (
+CREATE TABLE usuario (
     id INT AUTO_INCREMENT,
-    id_genero INT NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     correo VARCHAR(100) NOT NULL UNIQUE,
     contraseña VARCHAR(255) NOT NULL,
+    id_genero INT NOT NULL,
+    id_rol INT NOT NULL,
+
     PRIMARY KEY (id),
-    FOREIGN KEY (id_genero) REFERENCES genero(id_genero)
+    FOREIGN KEY (id_genero) REFERENCES genero(id_genero),
+    FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
 );
 
-CREATE TABLE administrador (
-    id INT NOT NULL,
-    id_administrador INT NOT NULL,
-	FOREIGN KEY (id) REFERENCES persona(id),
-    
-    PRIMARY KEY (id, id_administrador)
-);
 
 CREATE TABLE Producto (
     codigo_producto INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     descripcion TEXT,
-    precio DECIMAL(10, 2) NOT NULL
-);
-
-CREATE TABLE empleado (
-    id INT NOT NULL,
-    id_empleado INT NOT NULL,
-    id_cargo INT NOT NULL,
-    FOREIGN KEY (id) REFERENCES persona(id),
-    FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo),
-    PRIMARY KEY (id, id_empleado)
-);
-
-
-CREATE TABLE cliente (
-    id_cliente INT NOT NULL,
-    id INT NOT NULL,
-    direccion VARCHAR(50),
-    FOREIGN KEY (id) REFERENCES persona(id),
-    PRIMARY KEY (id_cliente,id)
-    
+    precio DECIMAL(10, 2) NOT NULL,
+    url_imagen VARCHAR(2083),
+    cantidad_disponible INT
 );
 
 CREATE TABLE ciudad(
@@ -69,26 +48,20 @@ CREATE TABLE ciudad(
 CREATE TABLE direccion (
     id_direccion INT NOT NULL,
     direccion VARCHAR(255) NOT NULL,  -- VARCHAR en lugar de TEXT
-    id_persona INT NOT NULL,
-    id_cliente INT NOT NULL,
     id_ciudad INT NOT NULL,
+    id_usuario INT NOT NULL,
     
     FOREIGN KEY (id_ciudad) REFERENCES ciudad(id_ciudad),
-    FOREIGN KEY (id_persona, id_cliente) REFERENCES cliente(id, id_cliente),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
     PRIMARY KEY (id_direccion)  -- Clave primaria compuesta
 );
 
-
-
-
 CREATE TABLE Pedido (
     id_pedido INT AUTO_INCREMENT NOT NULL, 
-    id_persona INT NOT NULL,
-    id_cliente INT NOT NULL,
     fecha DATE NOT NULL,
     total DECIMAL(10, 2),
-    FOREIGN KEY (id_persona,id_cliente) 
-		REFERENCES cliente(id,id_cliente),
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
 	PRIMARY KEY (id_pedido)
 );
 
@@ -100,67 +73,69 @@ CREATE TABLE Producto_Pedido (
     FOREIGN KEY (id_pedido) REFERENCES Pedido(id_pedido),
     FOREIGN KEY (codigo_producto) REFERENCES Producto(codigo_producto)
 );
+
+CREATE TABLE carrito (
+    id_usuario INT NOT NULL,
+    codigo_producto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_total_por_producto INT NOT NULL,
+
+    PRIMARY KEY (id_usuario, codigo_producto),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (codigo_producto) REFERENCES producto(codigo_producto)
+);
 -- La redundancia se reduci, y solo salio una tabla
 INSERT INTO genero (id_genero, genero) VALUES
 (1,'Masculino'),
-(2,'Femenino'),
-(3,'Helicoptero Apache')
+(2,'Femenino')
 ;
 
-INSERT INTO cargo (id_cargo, cargo) VALUES
-(1,'Vendedor'),
-(2,'Cajero'),
-(3,'Secretario');
+INSERT INTO rol (id_rol, rol) VALUES
+(1,'Administrador'),
+(2,'Cliente');
 
--- Insertar personas
-INSERT INTO persona (nombre, apellido, id_genero, correo, contraseña) VALUES
-('Juan', 'Pérez', 1, 'juan.perez@adminExample.com', 'contraseña1'),
-('Maria', 'Gómez', 2, 'maria.gomez@adminExample.com', 'contraseña2'),
-('Carlos', 'López', 1, 'carlos.lopez@Inadulcesexample.com', 'contraseña3'),
-('Ana', 'Martínez', 2, 'ana.martinez@Inadulcesexample.com', 'contraseña4'),
-('Luis', 'Fernández', 1, 'luis.fernandez@example.com', 'contraseña5'),
-('Sofía', 'Hernández', 2, 'sofia.hernandez@example.com', 'contraseña6');
 
--- Insertar administradores
-INSERT INTO administrador (id, id_administrador) VALUES
-(1, 1),  -- Juan Pérez como Administrador 1
-(2, 2);  -- Maria Gómez como Administradora 2
+-- Insertar usuarios
+INSERT INTO usuario (nombre, apellido, id_genero, correo, contraseña, id_rol) VALUES
+('Juan', 'Pérez', 1, 'juan.perez@adminExample.com', 'contraseña1',1),
+('Maria', 'Gómez', 2, 'maria.gomez@adminExample.com', 'contraseña2',1),
+('Carlos', 'López', 1, 'carlos.lopez@dminExample.com', 'contraseña3',1),
+('Ana', 'Martínez', 2, 'ana.martinez@dminExample.com', 'contraseña4',1),
+('Luis', 'Fernández', 1, 'luis.fernandez@example.com', 'contraseña5',2),
+('Sofía', 'Hernández', 2, 'sofia.hernandez@example.com', 'contraseña6',2);
 
--- Insertar empleados
-INSERT INTO empleado (id, id_empleado, id_cargo) VALUES
-(3, 1, 1),   -- Carlos López como Empleado 1
-(4, 2, 2);     -- Ana Martínez como Empleado 2
-
--- Insertar clientes
-INSERT INTO cliente (id, id_cliente,direccion) VALUES
-(5, 101,'Calle Falsa 123'),   -- Luis Fernández como Cliente 1
-(6, 102,'Avenida Siempre Viva'); -- Sofía Hernández como Cliente 2
-
+-- Insertar usuarios
 INSERT INTO ciudad (id_ciudad,nombre) VALUES
 (1,"Bogota"),
 (2,"Soacha");
 
-INSERT INTO direccion (id_direccion,direccion,id_persona,id_cliente,id_ciudad) VALUES
-(1,"Calle 7 #34 a 22",5,101,2),
-(2,"Calle 5 #12 B 27",6,102,1);
+INSERT INTO direccion (id_direccion, direccion,id_ciudad,id_usuario) VALUES
+(1,'Calle Falsa 123',1,1),
+(2,'Calle Falsa 456',1,2),
+(3,'Calle Falsa 789',1,3),
+(4,'Calle septima A #23-43',1,4),
+(5,'Calle novena # 12-15',2,5),
+(6,'Calle 4 norte #21-10',1,6);
 
 -- Insertar productos
-INSERT INTO Producto (nombre, descripcion, precio) VALUES
-('Masmellow A', 'Masmellow A sabor a A', 10.00),
-('Masmellow B', 'Masmellow A sabor a B', 15.50),
-('Masmellow C', 'Masmellow A sabor a C', 20.00),
-('Masmellow D', 'Masmellow A sabor a D', 25.75);
+INSERT INTO Producto (nombre, descripcion, precio,url_imagen,cantidad_disponible) VALUES
+('Masmellow A', 'Masmellow A sabor a AA', 3200,'',20),
+('Masmellow B', 'Masmellow A sabor a BB', 6400,'',50),
+('Masmellow C', 'Masmellow A sabor a CC', 7600,'',30),
+('Masmellow D', 'Masmellow A sabor a DD', 8500,'',10);
 
 -- Insertar pedidos
-INSERT INTO Pedido (id_persona,id_cliente, fecha, total) VALUES
-(5, 101, '2024-10-01', 35.50),   -- Pedido del Cliente 1
-(6, 102, '2024-10-02', 45.25),   -- Pedido del Cliente 2
-(5, 101, '2024-10-03', 30.00),   -- Pedido del Cliente 1
-(6, 102, '2024-10-04', 55.00);   -- Pedido del Cliente 2
+INSERT INTO Pedido (id_usuario, fecha, total) VALUES
+(5,'2024-10-01', 16000),   -- Pedido del usuario 1
+(6,'2024-10-02', 34000),   -- Pedido del usuario 2
+(5,'2024-10-03', 12800),   -- Pedido del usuario 1
+(6,'2024-10-04', 76000);   -- Pedido del usuario 2
 
 -- Insertar productos en pedidos
 INSERT INTO Producto_Pedido (codigo_producto, id_pedido, cantidad) VALUES
-(1, 1, 2),   -- Producto A en Pedido 1
-(2, 1, 1),   -- Producto B en Pedido 1
+(1, 1, 5),   -- Producto A en Pedido 1
+(4, 1, 3),   -- Producto B en Pedido 1
+(2, 1, 2),   -- Producto B en Pedido 1
 (3, 2, 3),   -- Producto C en Pedido 2
 (4, 2, 1);   -- Producto D en Pedido 2
+
