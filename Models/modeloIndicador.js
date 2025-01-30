f62ed49e88a6ef9a249244ed13bc4889c3858d5b
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2025-01-30 10:53:00 -0500

    Update modeloIndicador.js

--- Models/modeloIndicador.js
+++ Models/modeloIndicador.js
@@ -9,23 +9,21 @@ class Indicador{
         this.pedidosRealizados = pedidosRealizados;
     }
 
-    static async realizarConsulta (mes,annio){
+    static async realizarConsulta (){
        // const query = 'SELECT COUNT(*) FROM producto';
         const queries = {
             "producto": "SELECT COUNT(*) FROM producto;",
             "cliente": "SELECT COUNT(*) FROM usuario WHERE id_rol = 2;",
             "administrador": "SELECT COUNT(*) FROM usuario WHERE id_rol=1;",
-            "pedido": "SELECT COUNT(*) FROM factura WHERE YEAR(fecha) = ? AND MONTH(fecha) = ?;"
+            "pedido": "SELECT COUNT(*) FROM factura;"
         }
         const resultados = {}; //En este objeto, se guardaran los resultados de cada consulta.
         for (const consulta in queries) {
             console.log(`consulta: ${consulta}`)
             console.log(`Haiendo la consulta: ${queries[consulta]}`)
-            if(consulta == "pedido"){
-                resultados.consulta = await db.query(queries[consulta],[mes,annio]);                
-            }else{
-                resultados.consulta = await db.query(queries[consulta]);
-            }
+            
+            resultados.consulta = await db.query(queries[consulta]);
+            
             console.log(`Resultado de la consulta: ${resultados}`)
             
         }
